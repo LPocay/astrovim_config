84e585e207102c19cb28f43096b04a2bd14b8262
Luigi Pocay <lpocay@pop-os.localdomain>
2023-10-09 23:06:54 -0300

    Added first custom config

--- mappings.lua
+++ mappings.lua
@@ -1,24 +1,6 @@
--- Mapping data with "desc" stored directly by vim.keymap.set().
---
--- Please use this mappings table to set keyboard mapping since this is the
--- lower level configuration and more robust one. (which-key will
--- automatically pick-up stored data by this setting.)
 return {
-  -- first key is the mode
   n = {
-    -- second key is the lefthand side of the map
-
-    -- navigate buffer tabs with `H` and `L`
-    -- L = {
-    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-    --   desc = "Next buffer",
-    -- },
-    -- H = {
-    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-    --   desc = "Previous buffer",
-    -- },
-
-    -- mappings seen under group name "Buffer"
+    K = { function() vim.lsp.buf.hover() end, desc = "Hover symbol details" },
     ["<leader>bD"] = {
       function()
         require("astronvim.utils.status").heirline.buffer_picker(
@@ -27,14 +9,14 @@ return {
       end,
       desc = "Pick to close",
     },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<leader>y"] = { [["+y]], desc = "Copy to system clipboard" },
+  },
+  v = {
+    J = { ":m '>+1<CR>gv=gv" , desc =  "Move line down" },
+    K = { ":m '<-2<CR>gv=gv", desc = "Move line up" },
+    ["<leader>y"] = { [["+y]], desc = "Copy to system clipboard" },
   },
   t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
   },
 }
